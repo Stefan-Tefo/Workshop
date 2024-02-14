177b8bbe22e17155560aa0ef25ec46f508086966
Stefan-Tefo <150601798+Stefan-Tefo@users.noreply.github.com>
2024-02-14 19:17:06 +0100

    Homework

--- Workshop 3/app.js
+++ Workshop 3/app.js
@@ -1,21 +1,35 @@
 html = {
-    citySearchInput: document.querySelector("#citySearchInput"),
+    citySearchInput: document.getElementById("citySearchInput"),
     citySearchBtn: document.querySelector("#citySearchBtn"),
 }
 
-html.citySearchBtn.addEventListener("click", getTheCity())
+html.citySearchBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    getTheCity();
+
+})
+
 
 function getTheCity() {
-    let value = citySearchInput.value
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&appid=d18d33fa9d081450174592c8f6fe5cf`)
+    let value = html.citySearchInput.value
+    console.log("the value is " + value);
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=74e59f6374abe0d9b758877616ae444c`)
+        .then((res) => res.json())
+        .then((body) => {
+            console.log(body);
+            statistic(body)
+            console.log(statistic(body));
+        })
+}
+
+function statistic(body) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${body[0].lat}&lon=${body[0].lon}&appid=74e59f6374abe0d9b758877616ae444c`)
         .then((res) => res.json())
         .then((body) => {
             console.log(body);
         })
 }
 
-function statistic(body) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${body.lat}&lon=${body.lon}&appid=d18d33fa9d081450174592c8f6fe5cfd`)
-        .then((res) => res.json())
-
+function showWeatherAlert() {
+    return
 }
